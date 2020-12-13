904514212e785546a2c5ae39debf33ee2e808b1c
maxxyouu <max.you@outlook.com>
2020-12-13 14:36:48 -0500

    Part A 2 and 4

--- part_a/ensemble.py
+++ part_a/ensemble.py
@@ -1,1 +1,99 @@
-# TODO: complete this file.
+import os,sys,inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+from utils import load_train_csv, load_valid_csv, load_public_test_csv
+import numpy as np
+import matplotlib.pyplot as plt
+from item_response import \
+    irt as base_classifer, \
+    evaluate as base_evaluate, \
+    sigmoid # this is the base classifer
+
+def bootstrapping(data, num_learners=3):
+    """bootstrapping from the training dataset with replacement
+    :param data: A dictionary {user_id: list, question_id: list,
+    is_correct: list}
+    :param num_learners: number of base learner
+    :return a list of bootstrapped dataset (in dictionary form)
+    """
+    data['user_id'] = np.array(data['user_id'])
+    data['question_id'] = np.array(data['question_id'])
+    data['is_correct'] = np.array(data['is_correct'])
+
+    # bootstrap
+    bootstrapped_data = []
+    for _ in range(num_learners):
+        bootstrap = {}
+        # sample data with replacement
+        bootstrapped_indices = np.random.randint(low=0, high=len(data['user_id']), size=len(data['user_id']))
+        # check if sample with replacement =>  len(set(bootstrapped_indices)) < len(data['user_id'])
+        bootstrap['user_id'] = data['user_id'][bootstrapped_indices]
+        bootstrap['question_id'] = data['question_id'][bootstrapped_indices]
+        bootstrap['is_correct'] = data['is_correct'][bootstrapped_indices]
+        bootstrapped_data.append(bootstrap)
+    return bootstrapped_data
+
+def train_base_classifiers(bootstrap_data, val_data, lr=8, iteration=10): # lr=7, iteration=50
+    """train each base classifer
+
+    :param bootstrap_data: a list of bagged data set
+    :param val_data: validation data set
+    :param lr: the learning rate
+    :param iteration: the number of iteration to train each base model
+    :return a dictionary of trained base model
+    """
+    # train the base classifier
+    trained_models = {}
+    for model, bootstrap in enumerate(bootstrap_data):
+        _, _, opt_theta, opt_beta, validation_acc = base_classifer(bootstrap, val_data, lr, iteration)
+        trained_models[model] = [opt_theta, opt_beta, validation_acc]
+    return trained_models
+
+def bag_evaluate(data, trained_model):
+    predictions = []
+    for i, q in enumerate(data["question_id"]):
+        u = data["user_id"][i]
+        sum_prediction = 0
+
+        for model in trained_model:
+            theta, beta= trained_model[model][0], trained_model[model][1]
+            x = (theta[u] - beta[q]).sum()
+            p_a = sigmoid(x) # the prediction for this base classifier
+            correct = p_a >= 0.5
+            sum_prediction += correct
+
+        # get the bagged prediction
+        bagged_pred = sum_prediction / len(trained_model)
+        predictions.append(bagged_pred >= 0.5)
+
+    return np.sum((data["is_correct"] == np.array(predictions))) / len(data["is_correct"])
+
+def main():
+    train_data = load_train_csv("../data")
+    val_data = load_valid_csv("../data")
+    test_data = load_public_test_csv("../data")
+
+    bootstrapped_data = bootstrapping(train_data)
+    trained_models = train_base_classifiers(bootstrapped_data, val_data)
+
+    # bagged ensemble accuracy on validation and test set
+    valid_acc = bag_evaluate(val_data, trained_models)
+    test_acc = bag_evaluate(test_data, trained_models)
+    print('Ensemble validation accuracy: {}'.format(valid_acc))
+    print('Ensemble test accuracy: {}'.format(test_acc))
+
+    # base model accuracy on validation and test set
+    for i in trained_models:
+        theta, beta = trained_models[i][0], trained_models[i][1]
+        print('Base classifier {} validation accuracy: {}'.format(i, base_evaluate(val_data, theta, beta)))
+        print('Base classifier {} test accuracy: {}'.format(i, base_evaluate(test_data, theta, beta)))
+
+if __name__ == "__main__":
+    os.chdir(os.getcwd() + '/starter_code/part_a')
+    main()
+    # on average, ensemble is slightly better
+        # could be even better, but constrained by the correlation between dataset
+        # from the equation of the bagged variance, we know that it is more stable as long as there are some randomnesss
+    
--- part_a/item_response.py
+++ part_a/item_response.py
@@ -1,6 +1,12 @@
-from utils import *
+# from utils import *
+import os,sys,inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
 
+from utils import load_train_csv, load_valid_csv, load_public_test_csv, load_train_sparse
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def sigmoid(x):
@@ -20,42 +26,56 @@ def neg_log_likelihood(data, theta, beta):
     :param beta: Vector
     :return: float
     """
-    #####################################################################
-    # TODO:                                                             #
-    # Implement the function as described in the docstring.             #
-    #####################################################################
-    log_lklihood = 0.
-    #####################################################################
-    #                       END OF YOUR CODE                            #
-    #####################################################################
+    combined_data = zip(data['is_correct'], data['question_id'], data['user_id'])
+    log_lklihood = sum([cij * (theta[i] - beta[j]) - np.log(1 + np.exp(theta[i] - beta[j]))
+                        for cij, j, i in combined_data])
     return -log_lklihood
 
 
-def update_theta_beta(data, lr, theta, beta):
+def update_theta_beta(data_by_user, data_by_questions, unique_user_ids, unique_question_ids, lr, theta, beta):
     """ Update theta and beta using gradient descent.
+        note that use gradient descent since no close form solution
 
     You are using alternating gradient descent. Your update should look:
     for i in iterations ...
         theta <- new_theta
         beta <- new_beta
 
-    You may optionally replace the function arguments to receive a matrix.
-
-    :param data: A dictionary {user_id: list, question_id: list,
-    is_correct: list}
+    :param data_by_user: {user_id: list, question_id: list,
+    is_correct: list} sorted by users
+    :param data_by_questions: {user_id: list, question_id: list,
+    is_correct: list} sorted by question ids
+    :param unique_user_ids: a list of unique user ids in order
+    :param unique_question_ids: a list of unique question ids in order
     :param lr: float
     :param theta: Vector
     :param beta: Vector
     :return: tuple of vectors
     """
-    #####################################################################
-    # TODO:                                                             #
-    # Implement the function as described in the docstring.             #
-    #####################################################################
-    pass
-    #####################################################################
-    #                       END OF YOUR CODE                            #
-    #####################################################################
+    # gradient update for each theta
+    start_pos = 0
+    for i in unique_user_ids:
+        theta_i_gradients = []
+        for cij, j, i_prime in data_by_user[start_pos:]:
+            if i_prime != i:
+                break
+            jth = -cij + (np.exp(theta[i] - beta[j]) / (1 + np.exp(theta[i] - beta[j])))
+            theta_i_gradients.append(jth)
+        theta[i] = theta[i] - lr * (sum(theta_i_gradients) / len(theta_i_gradients))
+        start_pos += len(theta_i_gradients)
+    
+    # gradient updates for each beta
+    start_pos = 0
+    for j in unique_question_ids:
+        beta_j_gradients = []
+        for cij, j_prime, i in data_by_questions[start_pos:]:
+            if j_prime != j:
+                break
+            ith = cij - (np.exp(theta[i] - beta[j]) / (1 + np.exp(theta[i] - beta[j])))
+            beta_j_gradients.append(ith)
+        beta[j] = beta[j] - lr * (sum(beta_j_gradients) / len(beta_j_gradients))
+        start_pos += len(beta_j_gradients)
+
     return theta, beta
 
 
@@ -72,21 +92,38 @@ def irt(data, val_data, lr, iterations):
     :param iterations: int
     :return: (theta, beta, val_acc_lst)
     """
-    # TODO: Initialize theta and beta.
-    theta = None
-    beta = None
+    # Initialize theta and beta.
+    unique_questions, unique_users = sorted(set(data['question_id'])), sorted(set(data['user_id']))
+    data_by_user = sorted(zip(data['is_correct'], data['question_id'], data['user_id']), key=lambda item: (item[-1], item[1]))
+    data_by_questions = sorted(zip(data['is_correct'], data['question_id'], data['user_id']), key=lambda item: item[1:])
 
-    val_acc_lst = []
+    np.random.seed(311)
+    theta = np.random.uniform(0, 1, size=len(unique_users))
+    np.random.seed(412)# np.random.seed(311) 
+    beta = np.random.uniform(0, 1, size=len(unique_questions))
+    theta, beta = np.array(theta), np.array(beta)
+    
+    val_acc_lst, per_interation_params = [], []
+    train_lld, valid_lld = [], []
 
-    for i in range(iterations):
+    for i in range(iterations): # repeat until convergence
         neg_lld = neg_log_likelihood(data, theta=theta, beta=beta)
         score = evaluate(data=val_data, theta=theta, beta=beta)
-        val_acc_lst.append(score)
-        print("NLLK: {} \t Score: {}".format(neg_lld, score))
-        theta, beta = update_theta_beta(data, lr, theta, beta)
+        print("Iteration - {} NLLK: {} \t Score: {}".format(i, neg_lld, score))
 
-    # TODO: You may change the return values to achieve what you want.
-    return theta, beta, val_acc_lst
+        # for part b plot
+        train_lld.append(neg_lld)
+        valid_lld.append(neg_log_likelihood(val_data, theta=theta, beta=beta))
+
+        # store the parameters
+        val_acc_lst.append(score)
+        per_interation_params.append((theta, beta))
+        theta, beta = update_theta_beta(data_by_user, data_by_questions, unique_users, unique_questions, lr, theta, beta)
+
+    # get the parameters with the highest validation score
+    index = val_acc_lst.index(max(val_acc_lst))
+    optimal_theta, optimal_beta = per_interation_params[index]
+    return train_lld, valid_lld, optimal_theta, optimal_beta, max(val_acc_lst)
 
 
 def evaluate(data, theta, beta):
@@ -107,33 +144,62 @@ def evaluate(data, theta, beta):
     return np.sum((data["is_correct"] == np.array(pred))) \
            / len(data["is_correct"])
 
+def plot_log_likelihoods(train, validation, train_label, valid_label, figure_name, y_label):
+    iterations = [i for i in range(0, len(train))]
+    plt.plot(iterations, train, 'b-', label=train_label)
+    plt.plot(iterations, validation, 'g-', label=valid_label)
+    plt.legend(loc='upper right')
+    plt.xlabel('Iterations')
+    plt.ylabel(y_label)
+    plt.savefig(figure_name)
+    plt.close()
+
+def find_probabilities(theta, beta):
+    # randomly select 5 question
+    np.random.seed(311)
+    questions = np.random.randint(0, len(beta), 5)
+    sorted_theta = sorted(theta) # sort by the student's ability
+
+    probs = {}
+    for j in questions:
+        beta_prime = np.repeat(beta[j], len(theta))
+        question_correctness = np.exp(sorted_theta - beta_prime) / (1 + np.exp(sorted_theta - beta_prime))
+        probs[j] = question_correctness # the prediction trends is based on the sorted student ability
+    
+    colors = ['b-', 'g-', 'r-', 'm-', 'c-']
+    for color, j in enumerate(probs):
+        plt.plot(sorted_theta, probs[j], colors[color], label="question {}".format(j))
+
+    plt.legend(loc='upper right')
+    plt.xlabel('theta')
+    plt.ylabel('Probability of correctness')
+    plt.savefig('q2d.png')
+    plt.close()
 
 def main():
     train_data = load_train_csv("../data")
-    # You may optionally use the sparse matrix.
-    sparse_matrix = load_train_sparse("../data")
     val_data = load_valid_csv("../data")
     test_data = load_public_test_csv("../data")
 
-    #####################################################################
-    # TODO:                                                             #
-    # Tune learning rate and number of iterations. With the implemented #
-    # code, report the validation and test accuracy.                    #
-    #####################################################################
-    pass
-    #####################################################################
-    #                       END OF YOUR CODE                            #
-    #####################################################################
+    # learning_rate, iterations = 7, 50
+    learning_rate, iterations = 8, 20
 
-    #####################################################################
-    # TODO:                                                             #
-    # Implement part (c)                                                #
-    #####################################################################
-    pass
-    #####################################################################
-    #                       END OF YOUR CODE                            #
-    #####################################################################
+    train_lld, valid_lld, optimal_theta, optimal_beta, validation_acc = irt(train_data, val_data, learning_rate, iterations)
 
+    # part b
+    plot_log_likelihoods(train_lld, valid_lld, 
+                        'Train', 'Validation',
+                        'q2b.png', 'Negative log likelihood')
+    # c
+    test_acc = evaluate(test_data, optimal_theta, optimal_beta)
+    print('Validation accuracy: {}'.format(validation_acc))
+    print('Test accuracy: {}'.format(test_acc))
 
+    # d - select the first 5 questions and x axis is students
+    # find_probabilities(optimal_theta, optimal_beta)
+
+# Validation accuracy: 0.7071690657634773
+# Test accuracy: 0.7090036692068868
 if __name__ == "__main__":
+    os.chdir(os.getcwd() + '/starter_code/part_a')
     main()
